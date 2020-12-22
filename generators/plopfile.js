5bcf56dece9248cfd6a7045b50098485ac4c8fc5
Renato Oliveira <oliversieto@gmail.com>
2020-12-21 21:12:53 -0400

    chore: automatização criação de arquivos.

---
+++ generators/plopfile.js
@@ -0,0 +1,34 @@
+module.exports = function (plop) {
+  plop.setGenerator('component', {
+    description: 'application component logic',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'component name please'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'plop-templates/index.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/stories.tsx',
+        templateFile: 'plop-templates/stories.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/styles.ts',
+        templateFile: 'plop-templates/styles.ts.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/test.tsx',
+        templateFile: 'plop-templates/test.tsx.hbs'
+      }
+    ]
+  })
+}
